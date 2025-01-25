20532880a751dc3509bf6e6105ffc12275c31b42
debabin <dmitriybabinup@gmail.com>
2025-01-26 01:53:51 +0700

    main 🧊 change readme

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# **SHIFT WINTER ИНТЕНСИВ ⭐️**
-
-sample на html/css/js для frontend'а.
+# **shift intensive web sample ⭐️**
 
 ## Техническое задание
 
-- **📦 Доставка товаров** - [ссылка](https://omniscient-honeydew-f15.notion.site/7c46a3a33c8946bc8353d852afbf39a1)
-- **🍿 Кинотеатр** - [ссылка](https://omniscient-honeydew-f15.notion.site/90eef6b45a7843158d5731a1d4fd2440)
 - **🍕 Пицца** - [ссылка](https://omniscient-honeydew-f15.notion.site/efe624e43fa94004b37acc093565fa18)
-  
+- **🍿 Кинотеатр** - [ссылка](https://omniscient-honeydew-f15.notion.site/90eef6b45a7843158d5731a1d4fd2440)
+- **📦 Доставка товаров** - [ссылка](https://omniscient-honeydew-f15.notion.site/7c46a3a33c8946bc8353d852afbf39a1)
+
 ## Как работать ?
 
 На данной [странице](https://www.notion.so/2602fbc1bf3045b08a14bc0ae06304a9) вы можете найти ответы на вопросы по интенсиву
--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
-const app = document.getElementById("root");
+const app = document.getElementById('root');
 
 const init = async () => {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=151");
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151');
 
   const pokemons = (await response.json()).results;
 
-  const pokemonsContainer = document.createElement("div");
-  pokemonsContainer.className = "pokemons_container";
+  const pokemonsContainer = document.createElement('div');
+  pokemonsContainer.className = 'pokemons_container';
 
-  const pokemonsTitle = document.createElement("h1");
-  pokemonsTitle.innerHTML = "Pokemon first gen";
+  const pokemonsTitle = document.createElement('h1');
+  pokemonsTitle.innerHTML = 'Pokemons';
 
   app.appendChild(pokemonsTitle);
 
   for (let i = 0; i < pokemons.length; i++) {
     const pokemon = pokemons[i];
-    const pokemonContainer = document.createElement("div");
-    pokemonContainer.className = "pokemon";
+    const pokemonContainer = document.createElement('div');
+    pokemonContainer.className = 'pokemon';
 
-    const image = document.createElement("img");
+    const image = document.createElement('img');
     image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
       i + 1
     }.png`;
 
-    const name = document.createElement("div");
+    const name = document.createElement('div');
     name.innerHTML = pokemon.name;
 
     pokemonContainer.appendChild(image);
